a18642123e38cb6c87598f4169d3754838c267c3
Mig <mrigdhanendra@gmail.com>
2025-05-13 17:43:58 -0400

    completed part1 lab1

---
+++ labs/lab01/lab01p1.js
@@ -0,0 +1,26 @@
+import bob from "lodash";
+
+const holidays =[
+    {name: "Christmas", date: new Date("2025-12-25")},
+    {name: "Canada Day", date: new Date("2025-07-01")}, 
+    {name: "New Years", date: new Date("2026-01-01")}, 
+
+
+];
+
+let today = new Date();
+holidays.forEach(holiday => {
+   const difference = holiday.date - today;
+   console.log(Math.ceil(difference/(1000*60*60*24)))
+   console.log(`${holiday.name}is in ${days_till} days`)
+});
+
+const randomHoliday = bob.sample(holidays);
+console.log(randomHoliday);
+
+const index_christmas = bob.findIndex(holidays, {name: "Christmas"});
+const index_canada = bob.findIndex(holidays, {name: "Canada Day"});
+
+console.log(`Index of Christmas: ${index_christmas}`);
+console.log(`Index of Christmas: ${index_canada}`);
+
